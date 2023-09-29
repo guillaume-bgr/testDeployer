ad0e3ff68b6fcdbc751714ae79322d8b682a3ad2
GuillaumeBongrand <guillaumebongrand.lamanu@gmail.com>
2023-09-29 16:16:27 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'deployer/deployer' => array(
+            'pretty_version' => 'v7.3.1',
+            'version' => '7.3.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../deployer/deployer',
+            'aliases' => array(),
+            'reference' => 'c5c5e79d4e57445918ed24a9cdd3d85b0f261de3',
+            'dev_requirement' => true,
+        ),
+    ),
+);
